847e2c6802dbc6c998c7eb06231a2250b6c9df72
Anwar <anwarhussaini160@gmail.com>
2023-06-14 22:57:54 +0430

    Add Task 2

--- practice.test.js
+++ practice.test.js
@@ -6,3 +6,11 @@ const stringLength = (string) => string.length;
 test('hamid should return 5', () => {
   expect(stringLength('h')).toBe(1);
 });
+
+/* ******* *
+ * Task 01 *
+ ********* */
+const reverseString = string => [...string].reverse().join('');
+test('hamid should return dimad', () => {
+  expect(reverseString('hamid')).toBe('dimah');
+})
